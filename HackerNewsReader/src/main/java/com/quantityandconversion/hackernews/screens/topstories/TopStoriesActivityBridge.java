9aad34e67cb83336495a85b2be2038401d2096bd
Fyzxs <Fyzxs@users.noreply.github.com>
2017-03-24 17:27:47 -0700

    Android HackerNews App : Refactor to Clean Architecture - set up bridge interface

--- HackerNewsReader/src/main/java/com/quantityandconversion/hackernews/screens/topstories/TopStoriesActivityBridge.java
+++ HackerNewsReader/src/main/java/com/quantityandconversion/hackernews/screens/topstories/TopStoriesActivityBridge.java
@@ -3,12 +3,12 @@ package com.quantityandconversion.hackernews.screens.topstories;
 import com.quantityandconversion.hackernews.R;
 import com.quantityandconversion.utils.dialog.AlertDialogBuilder;
 
-/* package */ class TopStoriesActivityBridge {
+/* package */ class Bridge {
 
     private final TopStoriesActivity topStoriesActivity;
     private final TopItemsActivityMediator topItemsActivityMediator;
 
-    /* package */ TopStoriesActivityBridge(final TopStoriesActivity topStoriesActivity) {
+    /* package */ Bridge(final TopStoriesActivity topStoriesActivity) {
         if (topStoriesActivity == null) {
             throw new IllegalArgumentException("topStoriesActivity can not be null");
         }
@@ -19,15 +19,15 @@ import com.quantityandconversion.utils.dialog.AlertDialogBuilder;
 
     /* package */ Runnable dataError() {
         return () -> new AlertDialogBuilder<>()
-                .init(TopStoriesActivityBridge.this.topStoriesActivity)
+                .init(Bridge.this.topStoriesActivity)
                 .setTitle(R.string.top_stories_strings_alert_dialog_failure_title)
                 .setMessage(R.string.top_stories_strings_alert_dialog_failure_message)
                 .show();
     }
 
 
-    /* package */ Runnable dataLoad() {
-        return TopStoriesActivityBridge.this.topStoriesActivity::notifyTopStoriesChanged;
+    /* package */ Runnable dataChanged() {
+        return Bridge.this.topStoriesActivity::notifyTopStoriesChanged;
     }
 
     /* package */ void loadData() {
@@ -40,29 +40,5 @@ import com.quantityandconversion.utils.dialog.AlertDialogBuilder;
 
     /* package */  TopItemsAdapter createTopStoriesAdapter() {
         return new TopItemsAdapter(topItemsActivityMediator);
-    }
-
-    /* package */ static abstract class DataLoadStrategy{
-        /* package */ static final DataLoadStrategy DataChanged = new DataLoadStrategy(){
-            @Override
-            public void run(final TopStoriesActivityBridge bridge) {
-                bridge.topStoriesActivity.notifyTopStoriesChanged();
-            }
-        };
-
-        /* package */ static final DataLoadStrategy DataError = new DataLoadStrategy() {
-            @Override
-            public void run(final TopStoriesActivityBridge bridge) {
-
-                new AlertDialogBuilder<>()
-                        .init(bridge.topStoriesActivity)
-                        .setTitle(R.string.top_stories_strings_alert_dialog_failure_title)
-                        .setMessage(R.string.top_stories_strings_alert_dialog_failure_message)
-                        .show();
-            }
-        };
-
-        private DataLoadStrategy(){}
-        public abstract void run(final TopStoriesActivityBridge bridge);
     }
 }
